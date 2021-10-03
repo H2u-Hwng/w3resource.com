1c69433dddfbafacddb9ad864a2012c8d09ecf98
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2021-10-03 15:13:17 -0700

    Add files via upload

---
+++ python_exercises/python_basic_(part_1)/001. string_in_specific_format.py
@@ -0,0 +1,9 @@
+# The program displays the string in a specific format.
+
+# Display string in a specific format
+print('''Twinkle, twinkle, little star,
+        How I wonder what you are! 
+                Up above the world so high,
+                Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+        How I wonder what you are''')
